e340c23de34742584a2a35003acd3f61d5ea2b42
Ravi M Damor <damorravi540@gmail.com>
2021-08-09 15:18:27 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -5,6 +5,6 @@ public class sum {
        int num1 = 5, num2 = 15, sum;
        sum = num1 + num2;
  
-       System.out.println("Sum of these numbers: "+sum);
+       System.out.println("Sum of these two numbers: "+sum);
     }
  }
